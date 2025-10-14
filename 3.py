c8b51ba6c7b2f6a96e22fd08ed3f6f9e84d6c50b
이신우 <tlsdn0625@g.yju.ac.kr>
2025-10-14 09:19:17 +0900

    1010

---
+++ 3.py
@@ -0,0 +1,6 @@
+def sum(a, b):
+    return a + b
+
+print(sum(1, 2))
+print(sum(1, 1.0))
+print(sum("1", "1"))
---
+++ 4.py
@@ -0,0 +1,5 @@
+import re
+from typing import Union
+
+def sum(a: Union[int, float], b: Union[int, float]) -> int | float:
+    return a + b
